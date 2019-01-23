1dabc2b13784ddd3f46051741a789862d61fac4e
Unknown <disky32@naver.com>
2019-01-23 10:08:06 +0900

    로그인 정보 콘솔 출력 테스트

---
+++ spring-test/src/main/java/kr/green/springtest/MainController.java
@@ -0,0 +1,32 @@
+package kr.green.springtest;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class MainController {
+	
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String homeGet(Model model) {
+		return "home";
+	}
+	
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public String homePost(Model model, String id, String pw) {
+		System.out.println("id : "+id+", pw : " + pw);
+		return "redirect:/";
+	}
+	
+}
