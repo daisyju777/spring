e428b298e936382f95e945932b35e3a6ffe6dfde
Unknown <disky32@naver.com>
2019-01-23 12:08:16 +0900

    로그인 기능 구현- 유저정보 가져오기

--- spring-test/src/main/java/kr/green/springtest/MainController.java
+++ spring-test/src/main/java/kr/green/springtest/MainController.java
@@ -1,22 +1,21 @@
 package kr.green.springtest;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import kr.green.springtest.service.AccountService;
+import kr.green.springtest.vo.AccountVo;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class MainController {
-	
+	@Autowired
+	AccountService accountService;
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String homeGet(Model model) {
@@ -24,8 +23,9 @@ public class MainController {
 	}
 	
 	@RequestMapping(value = "/", method = RequestMethod.POST)
-	public String homePost(Model model, String id, String pw) {
-		System.out.println("id : "+id+", pw : " + pw);
+	public String homePost(Model model, AccountVo loginInfo) {
+		AccountVo user = accountService.signin(loginInfo); //accountService.signin(loginInfo)는 결과로 객체정보를 전달해줌
+		model.addAttribute("user",user);
 		return "redirect:/";
 	}
 	
